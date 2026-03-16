f302722c066b92cbf6553add8cfa2b1b71c9f1eb
superenova <erenn119@gmail.com>
2026-03-16 12:11:09 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace StudentApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool control = true;//while döngüsü true oldugu sürece sonsuz olacaktır. 
+            Student student1 = new Student(1663, "Baran", "Karakaya", 75, 50, 60, "Sakarya Üniversitesi");
+            //Öğrenci clasımdaki bilgilere ulaşmak için öğrenci clasından bir nesne ürettim.
+            //burada öncelikle öğrenci clasındaki parametre olarak belirtilen verilerin değerlerinin atamasını yapacağız.
+            //Bunu Student nesnesinin yanındaki parantezin içine yapıyoruz.
+            Console.WriteLine("Welcome To Application :) Make a Choice. ");
+            //okuma yaptıgı icin önce programa hosgeldiniz diyecek daha sonra da alttaki işlemleri sırasıyla gösterecek.)
+
+
+            while (control)
+            {
+                ShowTransactions();//while döngüsünde bu metotdu cagırırsam kullanıcı her secım yaptıktan sonra
+                //menü yü tekrar tekrar gösterecektir. while dışında cagırırsam bir defa gösterecektir.
+                string vote = Console.ReadLine(); //kullanıcı buradan bir seçim yapacak.
+                switch (vote) //örnegin if else kullanmak yerine switch case yaklaşımını kullanıyoruz.
+                {
+                    case "1":
+                        student1.ShowStudentİnformation();//ögrenci bilgileri göstermek icin yazılan metodu cagırmamız yeterli.
+                        break; //break = belirtilen secenekten bir digerine gecmek icin kullanılır. yani bir sonraki case "2" : olacak.
+                    case "2":
+                        double StudentAverage = student1.StudentGradePointAverage();//değişken double oldugu icin bir baska değişkene atadık.
+                        Console.WriteLine("Student Average :" + StudentAverage);
+                        break;
+                    case "3":
+                        student1.SchoolGet();
+                        break;
+                    case "4":
+                        control = false;//uygulamadan çıkması için yukarıda true belirttigim controlü burada false yapıyorum.
+                        break;
+
+                }
+            }
+        }
+        static void ShowTransactions() //Burada daha estetik görünmesi için seçeneklerin oldugu bir static metot oluşturdum.
+        {
+            //static olusturma sebebim sadece bu class ta işimi görecek ve bir referans tipi oluşturulmayacak.
+            Console.WriteLine("1-Show Student İnformation :");
+            Console.WriteLine("2-Show Student Average: ");
+            Console.WriteLine("3-Show Student School Name :");
+            Console.WriteLine("4-Exit Application ");
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApplication
+{
+    public class Student // class'ın erişim beliryecisi (public) diger class lardan görebilmek için.
+    {
+        //değişkenlerin erişim belirleyicileri private yaptık. Cünkü kontrollü bir şekilde verilerimi görmek istiyorum(Encapsulation)
+        private int StudentNo;
+        private string StudentName;//(string) demek yani sayısal olmayan metinsel ifadeler için kullanılıyor.
+        private string StudentSurname;
+        private int MidtermExam1;
+        private int MidtermExam2;
+        private int FinalExamination;
+        private string SchoolName;
+
+
+        public Student(int _StudentNo,string _StudentName,string _StudentSurname,int _MidtermExam1,int _MidtermEcam2,int _FinalExamination,string _SchoolName)
+        {
+            //degişkenler private olarak tanımlandı yukarıda. bu sekilde yapıcı metot aracılıgı ile Program cs clasımızda verilerimizi
+            //kendimiz girecegiz. ve yapıcı metot aracılıgı ile yukarıda belirtilen değişkenlere bu verileri atayacagız.
+            //Bu verileri dışarıdan alacagımız icin üstteki değişkenler ile karısmaması için önlerine alt tire simgesi koydum.
+            //dışarıdan alınan parametreler oldugunu belirtmek için.
+            //şimdi tanımlanan değişkenleri parametre olarak gelen değişkenlere eşitleyeceğiz.
+            StudentNo = _StudentNo;
+            StudentName = _StudentName;
+            StudentSurname = _StudentSurname;
+            MidtermExam1 = _MidtermExam1;
+            MidtermExam2 = _MidtermEcam2;
+            FinalExamination = _FinalExamination;
+            SchoolName = _SchoolName;
+        }
+
+        public void ShowStudentİnformation()
+        {
+            //(Void) geriye bir değer döndürmeyen yani return ifadesini kullanmadıgım metot icin kullanacagım tiptir.
+            Console.WriteLine("Student Number :"+ StudentNo);
+            Console.WriteLine("Student Name :"+StudentName);
+            Console.WriteLine("Student Surname :"+StudentSurname);
+            Console.WriteLine("Student Midterm Exam(1) :"+MidtermExam1);
+            Console.WriteLine("Student Midterm Exam(2)"+MidtermExam2);
+            Console.WriteLine("Student Final Examination :"+FinalExamination);
+            Console.WriteLine("Student School Name :"+SchoolName);
+        }
+
+        public double StudentGradePointAverage()
+        {
+            //burada öprencinin not ortalamasını bulacagız.
+            //ve (double) yapmamızın nedeni : öğrencinin not ortalaması tam sayı olmayabilir. örnegin 75,2 olabilir.
+            //bu nedenle int degil double yaptık.
+            double Average = MidtermExam1 * 0.2 + MidtermExam2 * 0.2 + FinalExamination * 0.6;
+            return Average;
+            //burada öğrencinin vize 1 notunu yüzde 20 si vize 2 nin yüzde 20 si ve final notunun yüzde 60 ını alıp topladık. 
+            //ve yine metotta belirttigimiz gibi double olarak bir ortalama(Average) adında değişkene bu işlemin sonucunu atadık.
+        }
+
+        public void SchoolGet() //farklı bir secenek olarak öğrenin okul adını getirecek metodu yazıyoruz.
+        {
+            Console.WriteLine("Student's School Name :"+SchoolName);
+        }
+    }
+}
